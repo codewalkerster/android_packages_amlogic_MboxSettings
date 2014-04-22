d7f5bdc01d1c4147e252d7af925f1743d97523f2
lei.qian <lei.qian>
2014-04-22 16:10:48 +0800

    pd #90490: don't close osd after adjust screen position.

--- src/com/mbx/settingsmbox/ScreenPositionManager.java
+++ src/com/mbx/settingsmbox/ScreenPositionManager.java
@@ -376,7 +376,7 @@ public class ScreenPositionManager {
           
 		}
         if(sw.getPropertyBoolean("ro.platform.has.realoutputmode", false)){
-            Utils.shadowScreen(sw, null);
+            //Utils.shadowScreen(sw, null);
             if((mCurrentMode.contains("1080")) && x.equals("0") && y.equals("0")){
                 writeFile(free_scale,"0x0");
                // Utils.setDisplaySize(w,h);
